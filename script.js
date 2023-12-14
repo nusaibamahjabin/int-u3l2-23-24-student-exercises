9bca4f23ab57816c2cd9ebe2ca5fdb569ac3cf78
nusaibamahjabin <147556039+nusaibamahjabin@users.noreply.github.com>
2023-12-14 22:00:28 +0000

    completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1. Take a look at these variables - how are they different than what we've seen?
-let paragraph = document.querySelector("p");
-let img = document.querySelector("img");
+let paragraph = "document.querySelector";
+let img = "document.querySelector";
 
 
 // 2. Remove the comment backslashes from the code below. What happened?
